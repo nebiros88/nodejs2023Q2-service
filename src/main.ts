3d55f53b73c411c19c9c386706437aaf8461de75
Siarhei Korbut <Siarhei.Korbut@ventionteams.com>
2023-08-20 17:49:50 +0200

    feat: create custom exception filter for error handling

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CustomExceptionFilter } from './utils/custom-exception-filter';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new CustomExceptionFilter());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
